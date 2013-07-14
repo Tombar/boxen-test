a91cb7dec8b3ece70171b1a16bf1d6642bebdfa0
Martin Loy <martin@guruhub.com.uy>
2013-07-14 01:10:01 -0300

    manifest for tombar

---
+++ modules/people/manifests/tombar.pp
@@ -0,0 +1,38 @@
+class people::tombar{
+
+  include alfred
+  include ctags
+  include chrome
+  include chrome::canary
+  include fish
+  include firefox
+  include geoip
+  include heroku
+  include iterm2::stable
+  include java
+  include jmeter
+  include notational_velocity::nvalt
+  include skype
+  include spectacle
+  include tmux
+  include tunnelblick
+  include virtualbox
+  include vagrant
+  include vlc
+  include wget
+
+  class { 'intellij':
+    edition => 'community',
+  }
+
+#   $home     = "/Users/${::luser}"
+#   $my       = "${home}/my"
+#   $dotfiles = "${my}/dotfiles"
+# 
+#   repository { $dotfiles:
+#     source  => 'jbarnette/dotfiles',
+#     require => File[$my]
+#   }
+ 
+
+}
